6c2a2897e4b3350d8b7be6af5a9954d8f414159a
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2022-12-21 15:25:38 +0900

    17.Array型

--- 02.Understanding-TS-course-starting-project/app.ts
+++ 02.Understanding-TS-course-starting-project/app.ts
@@ -5,6 +5,15 @@
 const person = {
     name: 'yota',
     age: 30,
+    hobbies: ['Sports','Cooking']
 }
 
+let favoliteActivities: string[]
+favoliteActivities = ['Sports']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+    // console.log(hobby.map()) => プロパティ 'map' は型 'string' に存在しません。と表示される。
+}
