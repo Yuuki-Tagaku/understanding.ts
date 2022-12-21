32bfb1aec0ae0f46a1aa4f14e4cd1206f1f5f0cf
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2022-12-21 17:48:17 +0900

    19.Enum型

--- 02.Understanding-TS-course-starting-project/app.js
+++ 02.Understanding-TS-course-starting-project/app.js
@@ -2,15 +2,44 @@
 //     name: string,
 //     age: number
 // }
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string],
+// } = {
+//     name: 'yota',
+//     age: 30,
+//     hobbies: ['Sports','Cooking'],
+//     role: [2, 'author'],
+// }
+// const ADMIN = 0
+// const READ_ONRY = 1
+// const AUTHOR =2
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONRY"] = 1] = "READ_ONRY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
-    name: 'yota',
+    name: "yota",
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN
 };
+// person.role.push('admin')
+// person.role[1] = 10
+// person.role = [0, 'admin', 'user']
+// Tuple型でより厳密に配列の型を指定してあげることができる
 var favoliteActivities;
-favoliteActivities = ['Sports'];
+favoliteActivities = ["Sports"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) => プロパティ 'map' は型 'string' に存在しません。と表示される。
+}
+if (person.role === Role.ADMIN) {
+    console.log("管理者ユーザー");
 }
