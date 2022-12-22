3848aa1ea1ff24d686af3ed88bcff18aa684d22b
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2022-12-22 10:59:52 +0900

    26.function型

--- 02.Understanding-TS-course-starting-project/app.js
+++ 02.Understanding-TS-course-starting-project/app.js
@@ -2,6 +2,10 @@ function add(n1, n2) {
     return n1 + n2;
 }
 function printRsult(num) {
-    console.log('Result: ' + num);
+    console.log("Result: " + num);
 }
+var combineValues;
+combineValues = add;
+// combineValues = printRsult
+console.log('combineValues', combineValues(8, 8));
 printRsult(add(5, 12));
--- 02.Understanding-TS-course-starting-project/app.ts
+++ 02.Understanding-TS-course-starting-project/app.ts
@@ -1,9 +1,16 @@
 function add(n1: number, n2: number) {
-    return n1 + n2
+  return n1 + n2
 }
 
 function printRsult(num: number): void {
-    console.log('Result: ' + num)
+  console.log("Result: " + num)
 }
+
+let combineValues: (a: number, b: number) => number
+
+combineValues = add
+// combineValues = printRsult
+
+console.log('combineValues', combineValues(8, 8))
 
 printRsult(add(5, 12))
