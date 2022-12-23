a5c09a1170a3bf63e029699efc7204744a451a85
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2022-12-23 13:42:18 +0900

    アロー関数

--- 04.Understanding-TS-next-gen-01-starting-setup/src/app.ts
+++ 04.Understanding-TS-next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,31 @@
-// Code goes here!
+const userName = 'Max'
+// userName = 'Maximilian' // constは定数のため値を変更できない
+let age = 30
+
+age = 29
+
+// varは使用しない　グローバルスコープとなるため
+
+const add = (a: number, b: number) => {
+  return a + b
+}
+
+const printOutput: (output: string | number) => void = (output) => {
+  console.log(output)
+}
+
+printOutput(add(2, 5))
+
+const button = document.querySelector('button')
+
+if (button) {
+  button.addEventListener('click', (event) => {
+    console.log(event)
+  })
+}
+
+// function add(a: number, b: number) {
+//   let result
+//   result = a + b
+//   return result
+// }
