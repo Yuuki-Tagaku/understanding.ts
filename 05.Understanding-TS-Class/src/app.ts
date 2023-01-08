dec3391fae05dbd01aa168e0d9b20b276944a841
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2023-01-08 11:51:25 +0900

    プロパティ初期化のショートカット構文

--- 05.Understanding-TS-Class/src/app.ts
+++ 05.Understanding-TS-Class/src/app.ts
@@ -1,21 +1,36 @@
 class Department {
-  name: string
+  // private id: string
+  // name: string
+  // pivateはそのクラスの中のメソッドからしかアクセスができないもの
+  private employees: string[] = []
 
-  constructor(n: string) {
-    this.name = n
+  constructor(private id: string, public name: string) {
+    // this.id = id
+    // this.name = n
   }
 
   describe(this: Department) {
-    console.log('Department: ' + this.name)
+    console.log(`Department (${this.id}): ${this.name}`)
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee)
+  }
+
+  printEmployeeInfomation() {
+    console.log(this.employees.length)
+    console.log(this.employees)
   }
 }
 
-const accounting = new Department('Accounting')
+const accounting = new Department('d1', 'Accounting')
 
-// console.log('accounting', accounting)
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
 
 accounting.describe()
+accounting.printEmployeeInfomation()
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
 
-accountingCopy.describe()
+// accountingCopy.describe()
